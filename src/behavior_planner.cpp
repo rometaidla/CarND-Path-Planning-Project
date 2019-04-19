06088766f3d7a582245ed51c51f64d5372b0b92f
Romet Aidla <romet.aidla@transferwise.com>
2019-04-19 15:04:19 +0000

    Separate constants file created

--- src/behavior_planner.cpp
+++ src/behavior_planner.cpp
@@ -1,6 +1,8 @@
-#include "behavior_planner.h"
 #include <iostream>
 #include <string>
+
+#include "behavior_planner.h"
+#include "constants.h"
 
 using std::string;
 using namespace std;
@@ -13,12 +15,13 @@ double BehaviorPlanner::calculateCost(vector<vector<double>> trajectory, vector<
 	
 	double cost = 50 - velocity;
 
-	bool too_close = false;
+
+	// COLLISION
 	double safety_distance = 30.0; // todo: make constant or better relative to vehicles speed
 	for (int i = 0; i < other_vehicles.size(); i++) {
 		Vehicle other_vehicle = other_vehicles[i];
 		if (other_vehicle.d < (2+4*lane+2) && other_vehicle.d > (2+4*lane-2)) {
-		    double other_vehicle_projected_s = other_vehicle.s + (double)prev_size * .02 * other_vehicle.velocity;
+		    double other_vehicle_projected_s = other_vehicle.s + (double)prev_size * SIMULATOR_DT * other_vehicle.velocity;
 		    double distance_to_car_in_front = other_vehicle_projected_s-car_s;
 		    if (other_vehicle_projected_s > car_s && distance_to_car_in_front < safety_distance) {
 		    	cout << "WARNING: vehicle on same lane: distance=" << distance_to_car_in_front << endl;
@@ -27,7 +30,8 @@ double BehaviorPlanner::calculateCost(vector<vector<double>> trajectory, vector<
 		}
 	}
 
-	if (velocity > 49.5) { // speed limit
+	// SPEED LIMIT
+	if (velocity > 49.5) {
 		cost += 100.0;
 	}
 
---
+++ src/constants.h
@@ -0,0 +1,6 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define SIMULATOR_DT 0.02
+
+#endif  // CONSTANTS_H
